b7087bcad555c7a42e3c0b44189f90e36808d9fe
SebastianValbuenaIng <134555752+SebastianValbuenaIng@users.noreply.github.com>
2023-05-31 10:33:24 -0500

    userdetails generado para obtener el correo del usuario que hace el login

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,18 +1,20 @@
 package com.example.demo;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello world";
+    public String hello() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        OidcUser userDetails = (OidcUser) authentication.getPrincipal();
+        String preferredUsername = userDetails.getPreferredUsername();
+        return String.format("Hello %s world", preferredUsername);
     }
 }
