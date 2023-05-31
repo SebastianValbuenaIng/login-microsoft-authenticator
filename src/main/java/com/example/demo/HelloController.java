ce49f17ab05399abbf7f06585f61a5a7364eb1a4
SebastianValbuenaIng <134555752+SebastianValbuenaIng@users.noreply.github.com>
2023-05-31 10:53:02 -0500

    cambiando codigo moderno userdetails

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        Authentication authentication = securityContext.getAuthentication();
-        OidcUser userDetails = (OidcUser) authentication.getPrincipal();
+        OidcUser userDetails = (OidcUser)  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String preferredUsername = userDetails.getPreferredUsername();
         return String.format("Hello %s world", preferredUsername);
     }
